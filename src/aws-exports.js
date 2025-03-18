54a9c52386d987dd7d42afefd4fcbbc7bb23e39d
mistikdiyebiri <ozalpp.mustafa@gmail.com>
2025-03-18 22:08:28 +0300

    AWS Amplify yapılandırması v5 formatına güncellendi

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,21 +2,10 @@
 // Bu yapılandırma AWS Amplify'ı local mock mod'da çalıştırmak içindir.
 
 const awsconfig = {
-    // Amplify v6 ile uyumlu konfigürasyon (MOCK MODE)
     aws_project_region: 'us-east-1',
-    Auth: {
-        // Kimlik doğrulama sistemi mock modda çalışacak
-        Cognito: {
-            userPoolId: 'mock-user-pool-id',
-            userPoolClientId: 'mock-client-id',
-            signUpVerificationMethod: 'code', // 'code', 'link'
-            loginWith: {
-                email: true,
-                phone: false,
-                username: false
-            }
-        }
-    }
+    aws_cognito_region: 'us-east-1',
+    aws_user_pools_id: 'mock-user-pool-id',
+    aws_user_pools_web_client_id: 'mock-client-id'
 };
 
-export default awsconfig; 
+export default awsconfig;
